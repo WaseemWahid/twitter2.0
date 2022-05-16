483b16cd5e513bc304e56af82843769c084b41a8
Waseem <wahid.waseem@gmail.com>
2022-05-16 14:54:06 -0700

    feed, twitter widget install and components.

---
+++ components/Widgets.tsx
@@ -0,0 +1,27 @@
+import { SearchIcon } from '@heroicons/react/outline'
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+import React from 'react'
+
+const Widgets = () => {
+    return (
+        <div className=" mt-2 px-2">
+            {/* Search */}
+            <div className="flex items-center space-x-2 rounded-full bg-gray-100 p-3 ">
+                <SearchIcon className="h-5 w-5 text-gray-400" />
+                <input
+                    type="text"
+                    placeholder="Search Twitter"
+                    className='flex-1 outline-none bg-transparent'
+                />
+            </div>
+
+            <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="elonmusk"
+                options={{ height: 1000 }}
+            />
+        </div>
+    )
+}
+
+export default Widgets
