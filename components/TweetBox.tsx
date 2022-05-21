e0ba588cfadf958a2181647c0b047c9429435293
Waseem <wahid.waseem@gmail.com>
2022-05-20 21:44:51 -0700

    schemas back end work done

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -21,10 +21,10 @@ const TweetBox = () => {
                     <div className='flex items-center'>
                         <div className="flex space-x-2 text-twitter flex-1">
                             <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
-                            <SearchCircleIcon className="h-5 w-5"/>
-                            <EmojiHappyIcon className="h-5 w-5"/>
-                            <CalendarIcon className="h-5 w-5"/>
-                            <LocationMarkerIcon className="h-5 w-5"/>
+                            <SearchCircleIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"/>
+                            <EmojiHappyIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"/>
+                            <CalendarIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"/>
+                            <LocationMarkerIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"/>
                         </div>
                         <button disabled={!input} className='rounded-full bg-twitter px-5 py-2 font-bold text-white disabled:opacity-40'>Tweet</button>
                     </div>
