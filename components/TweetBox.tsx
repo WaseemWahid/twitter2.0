407684994f17a786d638f50210de6df7a032f94a
Waseem <wahid.waseem@gmail.com>
2022-05-27 23:06:17 -0700

    Implemented the Image URL Functionality

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,10 +1,25 @@
 import { CalendarIcon, EmojiHappyIcon, LocationMarkerIcon, PhotographIcon, SearchCircleIcon } from '@heroicons/react/outline'
 import { useSession } from 'next-auth/react'
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 const TweetBox = () => {
     const [input, setInput] = useState<string>("")
+    const [image, setImage] = useState<string>("")
+
+    const imageInputRef = useRef<HTMLInputElement>(null)
+
     const { data: session } = useSession()
+    const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState<boolean>(false)
+
+    const addImageToTweet = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        if (!imageInputRef.current?.value) return;
+
+        setImage(imageInputRef.current.value)
+        imageInputRef.current.value = '';
+        setImageUrlBoxIsOpen(false);
+    }
 
     return (
         <div className='flex space-x-2 p-5'>
@@ -22,7 +37,7 @@ const TweetBox = () => {
                     />
                     <div className='flex items-center'>
                         <div className="flex space-x-2 text-twitter flex-1">
-                            <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+                            <PhotographIcon onClick={() => setImageUrlBoxIsOpen(!imageUrlBoxIsOpen)} className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
                             <SearchCircleIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"/>
                             <EmojiHappyIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"/>
                             <CalendarIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"/>
@@ -30,6 +45,26 @@ const TweetBox = () => {
                         </div>
                         <button disabled={!input || !session} className='rounded-full bg-twitter px-5 py-2 font-bold text-white disabled:opacity-40'>Tweet</button>
                     </div>
+                    {imageUrlBoxIsOpen && (
+                        <form className='mt-5 flex rounded-lg bg-twitter/80 py-2 px-4'>
+                            <input 
+                                ref={imageInputRef}
+                                className='flex-1 bg-transparent p-2 text-white outline-none placeholder:text-white'
+                                type="text" 
+                                placeholder='Enter Image URL...'
+                            />
+                            <button type='submit' onClick={addImageToTweet} className='font-bold text-white'>
+                                Add Image
+                            </button>
+                        </form>
+                    )}
+                    {image && 
+                        <img 
+                            className='mt-10 h-40 w-full rounded-xl object-contain shadow-lg' 
+                            src={image} 
+                            alt='' 
+                        />
+                    }
                 </form>
             </div>
         </div>
