b77ee87a695921afef8e2a80d03e1e2252783dba
Waseem <wahid.waseem@gmail.com>
2022-05-27 22:36:01 -0700

    implemented login functionality with nextAuth

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,13 +1,15 @@
 import { CalendarIcon, EmojiHappyIcon, LocationMarkerIcon, PhotographIcon, SearchCircleIcon } from '@heroicons/react/outline'
+import { useSession } from 'next-auth/react'
 import React, { useState } from 'react'
 
 const TweetBox = () => {
     const [input, setInput] = useState<string>("")
+    const { data: session } = useSession()
 
     return (
         <div className='flex space-x-2 p-5'>
             <img className="mt-4 h-14 w-14 object-cover rounded-full" 
-            src="https://links.papareact.com/gll"
+            src={session?.user?.image || "https://links.papareact.com/gll"}
             alt=""/>
             <div className='flex-1 flex items-center pl-2'>
                 <form className='flex flex-1 flex-col'>
@@ -26,7 +28,7 @@ const TweetBox = () => {
                             <CalendarIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"/>
                             <LocationMarkerIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"/>
                         </div>
-                        <button disabled={!input} className='rounded-full bg-twitter px-5 py-2 font-bold text-white disabled:opacity-40'>Tweet</button>
+                        <button disabled={!input || !session} className='rounded-full bg-twitter px-5 py-2 font-bold text-white disabled:opacity-40'>Tweet</button>
                     </div>
                 </form>
             </div>
